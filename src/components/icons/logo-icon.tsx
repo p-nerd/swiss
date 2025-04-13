def99028064a78ce33d3ffd6dc3637468280b033
Shihab Mahamud <shihab4t@gmail.com>
2025-04-13 16:25:37 +0600

    hero component pettern done

--- src/components/icons/logo-icon.tsx
+++ src/components/icons/logo-icon.tsx
@@ -1,7 +1,7 @@
 export const LogoIcon = () => {
     return (
         <div className="flex items-center">
-            <div className="relative h-10 w-10 overflow-hidden rounded bg-red-600">
+            <div className="relative h-10 w-10 overflow-hidden rounded bg-primary">
                 <div className="absolute inset-0 flex items-center justify-center text-white font-bold text-4xl">
                     S
                 </div>
