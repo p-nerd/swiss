0586579122dc8b3ca6f0ccc1778d4fcfb870a33a
Shihab Mahamud <shihab4t@gmail.com>
2025-04-13 16:59:14 +0600

    add icons to the pages

---
+++ src/components/icons/icons-list.tsx
@@ -0,0 +1,8 @@
+import { CalculatorIcon, CropIcon, FileTextIcon, KeyIcon } from "lucide-react";
+
+export const iconsList = {
+    "key-icon": KeyIcon,
+    "file-text-icon": FileTextIcon,
+    "calculator-icon": CalculatorIcon,
+    "crop-icon": CropIcon
+};
